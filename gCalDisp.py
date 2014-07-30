842562cb9874936bfbb12ab8648022df6cdd977d
Mike S <Mike@itll-hv04.itll.cu.int>
2014-07-30 15:34:31 -0600

    Clean up and comment python

--- gCalDisp.py
+++ gCalDisp.py
@@ -1,32 +1,50 @@
+# This script checks in a google calendar for the current event and then parses the
+# text to currently print to the terminal.
+
+# This is my calendar...
 calendarId = 'lkqtiiehlj121aqp0a7g3ma25k@group.calendar.google.com'
 
-# pip install --upgrade google-api-python-client
+# Note: pip install --upgrade google-api-python-client installed to the wrong path,
+# So this is just a hack to tell python where to find it...
 import sys
 sys.path.append("/usr/local/lib/python2.7/site-packages")
+
 import google_calendar
 import pprint
+# Note: If i decide to talk via serial commands, I'll need this...
 # import serial
 import time
 import datetime
-#import dateutil.parser
 
 
 def getEvents(pageToken=None):
+    # Get the current date and time
     now = datetime.datetime.utcnow();
-    tomorrow = now + datetime.timedelta(days=1)
+    future = now + datetime.timedelta(minutes=1)
+    
+    # for debugging, print now and future to terminal.
     print now.isoformat()
-    print tomorrow.isoformat()
-  #query.start_min = now.isoformat()
-  #query.start_max = tomorrow.isoformat()
+    print future.isoformat()
 
+    #query google_calendar for current event
     page_token = None
-    while True:
-      events = google_calendar.service.events().list(
-        calendarId= calendarId
+    events = google_calendar.service.events().list(
+        calendarId= calendarId,
         timeMin=now.isoformat()+'z',
-        timeMax=tomorrow.isoformat()+'z',
+        timeMax=future.isoformat()+'z',
         pageToken=page_token
         ).execute()
+    
+    print "Hello World 1"
+    return events
+
+def main():
+    # Get Current Event
+    events = getEvents()
+
+    while True:
+      # Print out current event summary, location, and end time. If any of those 
+      # fields does not exist, ignore the KeyError exception and move along.
       for event in events['items']:
         print 'Prof Mike Soltys is:'
         try:
@@ -40,43 +58,20 @@ def getEvents(pageToken=None):
           continue
         print 'Until:'    
         endTime = event['end']['dateTime']
-        endTime = endTime[:-6] #Need to strip timezone info
+        # Note: endTime is in iso format with timezone info, we just need to strip
+        # the last 6 digits to get the current local time
+        endTime = endTime[:-6] 
+        # Print Time in 12H:MM AM/PM format...
         ET=datetime.datetime.strptime(endTime, "%Y-%m-%dT%H:%M:%S")
         print ET.strftime("%I:%M%p")
 
+      # If there is more than one event, then currently the code will loop through 
+      # all events that match the query and do the same.
       page_token = events.get('nextPageToken')
       if not page_token:
         break
 
-
-    calendar = google_calendar.service.calendars().get(
-        calendarId=calendarId).execute()
-
-    #print calendar['summary']
-    #print 'hello world'
-    # events = google_calendar.service.events().list(
-    #     calendarId='lkqtiiehlj121aqp0a7g3ma25k@group.calendar.google.com',
-    #     singleEvents=True,
-    #     maxResults=1000,
-    #     orderBy='startTime',
-    #     timeMin='2014-11-01T00:00:00-08:00',
-    #     timeMax='2014-11-30T00:00:00-08:00',
-    #     pageToken=pageToken,
-    #     ).execute()
-    # return events
-    return calendar
-
-def main():
-    #print 'hello world 1'
-     events = getEvents()
-    # while True:
-    #     for event in events['items']:
-    #         pprint.pprint(event)
-    #     page_token = events.get('nextPageToken')
-    #     if page_token:
-    #         events = getEvents(page_token)
-    #     else:
-    #         break
-
+# This is just nice to have because it can tell the terminal 
+# running python where to find main()
 if __name__ == '__main__':
     main()
