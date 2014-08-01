a402fe8b3ea5053f7e646906c9e1d4fdeebf314f
Mike S <mrsoltys@gmail.com>
2014-08-01 16:06:27 -0600

    Add LCD display support

--- gCalDisp.py
+++ gCalDisp.py
@@ -15,7 +15,7 @@ import pprint
 # import serial
 import time
 import datetime
-
+import lcd_display
 
 def getEvents(pageToken=None):
     # Get the current date and time
@@ -35,41 +35,35 @@ def getEvents(pageToken=None):
         pageToken=page_token
         ).execute()
     
-    print "Hello World 1"
     return events
 
 def main():
+    lcd_display.main()
     # Get Current Event
-    events = getEvents()
-
     while True:
-      # Print out current event summary, location, and end time. If any of those 
-      # fields does not exist, ignore the KeyError exception and move along.
+      events = getEvents()
+
+
+    # Print out current event summary, location, and end time. If any of those 
+    # fields does not exist, ignore the KeyError exception and move along.
       for event in events['items']:
         print 'Prof Mike Soltys is:'
         try:
           print event['summary']
-        except KeyError:
-          continue
-        print 'At:'  
-        try:
-          print event['location']
-        except KeyError:
-          continue
-        print 'Until:'    
-        endTime = event['end']['dateTime']
+          lcd_display.lcd_byte(lcd_display.LCD_LINE_1, lcd_display.LCD_CMD)
+ 	  lcd_display.lcd_string(event['summary'])
+          endTime = event['end']['dateTime']
         # Note: endTime is in iso format with timezone info, we just need to strip
         # the last 6 digits to get the current local time
-        endTime = endTime[:-6] 
+          endTime = endTime[:-6] 
         # Print Time in 12H:MM AM/PM format...
-        ET=datetime.datetime.strptime(endTime, "%Y-%m-%dT%H:%M:%S")
-        print ET.strftime("%I:%M%p")
-
-      # If there is more than one event, then currently the code will loop through 
-      # all events that match the query and do the same.
-      page_token = events.get('nextPageToken')
-      if not page_token:
-        break
+          ET=datetime.datetime.strptime(endTime, "%Y-%m-%dT%H:%M:%S")
+          print ET.strftime("%I:%M%p")
+          lcd_display.lcd_byte(lcd_display.LCD_LINE_2, lcd_display.LCD_CMD)
+          lcd_display.lcd_string('Until: '+ET.strftime("%I:%M%p"))
+        except KeyError:
+          continue
+      time.sleep(60)
 
 # This is just nice to have because it can tell the terminal 
 # running python where to find main()
--- google_calendar.py
+++ google_calendar.py
@@ -21,7 +21,7 @@ FLOW = OAuth2WebServerFlow(
     user_agent='YOUR_APPLICATION_NAME/YOUR_APPLICATION_VERSION')
 
 # To disable the local server feature, uncomment the following line:
-# FLAGS.auth_local_webserver = False
+FLAGS.auth_local_webserver = True
 
 # If the Credentials don't exist or are invalid, run through the native client
 # flow. The Storage object will ensure that if successful the good
